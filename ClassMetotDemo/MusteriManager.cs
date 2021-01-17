74bd750242225ff206dfb05b8c6cb441ce4d15df
Zafer Yazıcı <zafer.yazici@senkron.com.tr>
2021-01-17 16:04:40 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+
+
+
+        public static void MusteriEkle(Musteri musteri)
+        {
+
+
+            Console.WriteLine(musteri.Adi + " " +musteri.Soyadi + " Yaşı : " + musteri.Yasi +" Musteri Eklenmiştir");
+
+
+
+
+        }
+
+        public static void MusteriListele(Musteri[] musteri)
+        {
+
+
+            foreach (Musteri musterilerimiz in musteri)
+            {
+
+                Console.WriteLine(musterilerimiz.Adi + " " + musterilerimiz.Soyadi + " " +  musterilerimiz.Yasi +  " Musteri Listelenmiştir");
+            }
+
+
+        }
+        public static void MusteriSilme(Musteri musteri)
+        {
+
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " Musteri Silinmiştir");
+
+        }
+
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Musteri musteri1 = new Musteri();
+
+            musteri1.Adi = "ZAFER";
+            musteri1.Soyadi = "YAZICI";
+            musteri1.Yasi = 35;
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.Adi = "ALİ";
+            musteri2.Soyadi = "CAN";
+            musteri2.Yasi = 45;
+
+            Musteri musteri3 = new Musteri();
+
+            musteri3.Adi = "AYŞE";
+            musteri3.Soyadi = "SARI";
+            musteri3.Yasi = 23;
+
+            Musteri[] musteris = new Musteri[] { musteri1, musteri2, musteri3 };
+
+
+            MusteriManager.MusteriEkle(musteri3);
+            MusteriManager.MusteriListele(musteris);
+            MusteriManager.MusteriSilme(musteri1);
+
+
+
+
+
+
+            
+        }
+    }
+}
